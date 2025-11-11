fbd941b7cf6e46d2ed282d305b2278cd70253457
gracyaneoliveira <gracyane_oliveira@hotmail.com>
2025-11-11 00:27:16 -0300

    8.37. Desafio: Implemente persistência para Shopping Cart

--- src/main/java/com/algaworks/algashop/ordering/domain/model/entity/ShoppingCart.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/entity/ShoppingCart.java
@@ -23,11 +23,14 @@ public class ShoppingCart implements AggregateRoot<ShoppingCartId>{
     private OffsetDateTime createdAt;
     private Set<ShoppingCartItem> items;
 
+    private Long version;
+
     @Builder(builderClassName = "ExistingShoppingCartBuilder", builderMethodName = "existing")
-    public ShoppingCart(ShoppingCartId id, CustomerId customerId,
+    public ShoppingCart(ShoppingCartId id, Long version, CustomerId customerId,
                         Money totalAmount, Quantity totalItems, OffsetDateTime createdAt,
                         Set<ShoppingCartItem> items) {
         this.setId(id);
+        this.setVersion(version);
         this.setCustomerId(customerId);
         this.setTotalAmount(totalAmount);
         this.setTotalItems(totalItems);
@@ -36,7 +39,7 @@ public class ShoppingCart implements AggregateRoot<ShoppingCartId>{
     }
 
     public static ShoppingCart startShopping(CustomerId customerId) {
-        return new ShoppingCart(new ShoppingCartId(), customerId, Money.ZERO,
+        return new ShoppingCart(new ShoppingCartId(), null, customerId, Money.ZERO,
                 Quantity.ZERO, OffsetDateTime.now(), new HashSet<>());
     }
 
@@ -133,6 +136,10 @@ public class ShoppingCart implements AggregateRoot<ShoppingCartId>{
         return createdAt;
     }
 
+    public Long version() {
+        return version;
+    }
+
     private void updateItem(ShoppingCartItem shoppingCartItem, Product product, Quantity quantity) {
         shoppingCartItem.refresh(product);
         shoppingCartItem.changeQuantity(shoppingCartItem.quantity().add(quantity));
@@ -190,6 +197,10 @@ public class ShoppingCart implements AggregateRoot<ShoppingCartId>{
     private void setItems(Set<ShoppingCartItem> items) {
         Objects.requireNonNull(items);
         this.items = items;
+    }
+
+    private void setVersion(Long version) {
+        this.version = version;
     }
 
     @Override
