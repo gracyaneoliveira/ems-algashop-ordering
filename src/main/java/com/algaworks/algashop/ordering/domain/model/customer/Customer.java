6ee15b398243f87380c26cd2448506437f3d1fb3
gracyaneoliveira <gracyane_oliveira@hotmail.com>
2025-11-17 21:36:54 -0300

    13.4. Implementando o evento de Customer Archived

--- src/main/java/com/algaworks/algashop/ordering/domain/model/customer/Customer.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/customer/Customer.java
@@ -92,6 +92,8 @@ public class Customer
         this.setAddress(this.address().toBuilder()
                 .number("Anonymized")
                 .complement(null).build());
+
+        this.publishDomainEvent(new CustomerArchivedEvent(this.id(), this.archivedAt()));
     }
 
     public void enablePromotionNotifications() {
